b41e2e4100a2407d3a602e5d89fbb7f51951a73d
ring-wdr <akswnd55@gmail.com>
2023-12-03 01:49:17 +0900

    config: about mongodb
    - fix configuration of mongodb due to example on official example

--- src/app/admin/action.ts
+++ src/app/admin/action.ts
@@ -7,30 +7,22 @@ import { OrderItem } from "@/type";
 
 export const crawlDataFromExternal = async (_: string[], data: FormData) => {
   const { masterKey } = Object.fromEntries(data);
-  if (masterKey !== "admin123") return ["크롤링 권한이 없습니다."];
-  try {
-    await client.connect();
-    const db = client.db(dbname);
-    const menuCollection = db.collection(menu);
-    menuCollection.deleteMany({});
-    const data = await getMenu("https://www.coffeebeankorea.com/menu/list.asp");
-    const res = await menuCollection.insertMany(data);
-    return Object.entries(res.insertedIds).map(([_, value]) =>
-      value.toString()
-    );
-  } finally {
-    client.close();
-  }
+  if (masterKey !== process.env.ADMIN_PASSWORD)
+    return ["크롤링 권한이 없습니다."];
+
+  const db = (await client).db(dbname);
+  const menuCollection = db.collection(menu);
+  menuCollection.deleteMany({});
+  const menuList = await getMenu(
+    "https://www.coffeebeankorea.com/menu/list.asp"
+  );
+  const res = await menuCollection.insertMany(menuList);
+  return Object.entries(res.insertedIds).map(([_, value]) => value.toString());
 };
 
 export const getOrderedList = async () => {
-  try {
-    await client.connect();
-    const db = client.db(dbname);
-    const orderCollection = db.collection<OrderItem>(order);
-    const orders = await orderCollection.find().toArray();
-    return orders.map((order) => ({ ...order, _id: order._id.toString() }));
-  } finally {
-    client.close();
-  }
+  const db = (await client).db(dbname);
+  const orderCollection = db.collection<OrderItem>(order);
+  const orders = await orderCollection.find().toArray();
+  return orders.map((order) => ({ ...order, _id: order._id.toString() }));
 };
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,42 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const client = new MongoClient(process.env.NEXT_PUBLIC_MONGO_KEY!, {
+if (!process.env.NEXT_PUBLIC_MONGO_KEY) {
+  throw new Error(
+    'Invalid/Missing environment variable: "NEXT_PUBLIC_MONGO_KEY"'
+  );
+}
+
+const uri = process.env.NEXT_PUBLIC_MONGO_KEY;
+const options = {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   },
   writeConcern: { w: "majority" },
-});
-export default client;
+} as const;
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>;
+  };
+
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    globalWithMongo._mongoClientPromise = client.connect();
+  }
+  clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise;
