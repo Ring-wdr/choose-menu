41360396360f4a0f1c8d7bb23f613bb953f9bda2
ring-wdr <akswnd55@gmail.com>
2023-12-09 15:02:33 +0900

    refactor: bottomsheet to compound pattern

--- src/component/BottomSheet/index.tsx
+++ src/component/BottomSheet/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, {
-  useState,
   useEffect,
   useRef,
   useCallback,
@@ -15,31 +14,154 @@ import { createPortal, useFormStatus } from "react-dom";
 import { useScreenSize } from "@/hooks/useScreenSize";
 import executeAnimate from "@/hooks/executeAnimate";
 import Button from "@/component/Button";
-import clsx from "clsx";
 import bsStyles from "./sheet.module.css";
 
 type BottomSheetProps = {
   isOpen: boolean;
-  onToggle?: (input: boolean) => void;
-} & React.PropsWithChildren;
+  onClose?: () => void;
+  /** 퍼센트(%, '20%') 입력시 비율, 숫자 입력시 고정 픽셀(px)
+   * ```
+   * 0%: 초기 위치보다 아래인 경우 닫기
+   * 50%: 화면 중간 아래인 경우 닫기
+   * 100%: 화면 아래인 경우 닫기
+   * ```
+   */
+  initPosition?: number | `${number}%`;
+  /** 바텀 시트가 몇 프로 이하로 내려갔을 때 닫게 하는지 결정 */
+  closePosition?: `${number}%`;
+  breakPosition?: Array<number | `${number}%`>;
+  closeWhenBackdropClick?: boolean;
+};
 
 type BottomSheetCtxProps = {
-  isOpen: boolean;
-  onOpen?: () => void;
-  onClose?: () => void;
-};
-export const BottomSheetContext = createContext<BottomSheetCtxProps>({
+  onCloseAction?: (e?: ReactMouseEvent<HTMLElement>) => void;
+  portalRef?: React.MutableRefObject<HTMLDivElement>;
+  dragRef?: React.RefObject<HTMLDivElement>;
+  dragStateOn?: () => void;
+  dragStateOff?: () => void;
+  closeDragElement?: () => void;
+  topPosition: number;
+  bodyHeight: number;
+} & Required<Omit<BottomSheetProps, "onClose">>;
+
+const bottomSheetInitValue = {
   isOpen: false,
-});
+  topPosition: 0,
+  initPosition: "20%",
+  closePosition: "50%",
+  bodyHeight: 0,
+  breakPosition: [],
+  closeWhenBackdropClick: true,
+} satisfies BottomSheetCtxProps;
+
+export const BottomSheetContext =
+  createContext<BottomSheetCtxProps>(bottomSheetInitValue);
 const useBottomSheetContext = () => useContext(BottomSheetContext);
 
-function BottomSheetlMain({ children, onToggle, isOpen }: BottomSheetProps) {
-  const [{ onOpen, onClose }] = useState(() => ({
-    onOpen: () => onToggle && onToggle(true),
-    onClose: () => onToggle && onToggle(false),
-  }));
+function BottomSheetlMain({
+  isOpen,
+  onClose,
+  children,
+  initPosition = "20%",
+  closePosition = "50%",
+  breakPosition = [],
+  closeWhenBackdropClick = true,
+}: React.PropsWithChildren<BottomSheetProps>) {
+  const portalRef = useRef(document.createElement("div"));
+  /** control handle state */
+  const dragRef = useRef<HTMLDivElement>(null);
+  const dragStateOn = () => {
+    if (dragRef.current) dragRef.current.dataset.drag = "true";
+  };
+  const dragStateOff = () => {
+    if (dragRef.current) dragRef.current.dataset.drag = "false";
+  };
+  /** 페이지의 크기 */
+  const bodyHeight = useScreenSize();
+  const topPosition = getPosition(initPosition, bodyHeight);
+  const onCloseAction = useCallback(
+    (e?: ReactMouseEvent<HTMLElement>) => {
+      if (!dragRef || (e && e.target !== e.currentTarget)) return;
+      /** 현재 위치 */
+      const currentTopPosition = Number(getBSPosition(dragRef));
+      document.body.style.removeProperty("overflow");
+      executeAnimate(
+        currentTopPosition,
+        (position) => (setBSPosition(dragRef, `${position}px`), position + 30),
+        (position) => position < document.body.clientHeight
+      );
+      onClose && setTimeout(onClose, 300);
+    },
+    [onClose, dragRef]
+  );
+  const closeDragElement = () => {
+    if (!dragRef || dragRef.current === null) return;
+    dragStateOff();
+    const dividePercentage = Number(closePosition.slice(0, -1)) / 100;
+    /** 현재 위치 */
+    const currentTopPosition = Number(getBSPosition(dragRef));
+    /** 바텀 시트가 닫히는 기준 위치 */
+    const standardClosePosition =
+      bodyHeight * dividePercentage + topPosition * (1 - dividePercentage);
+
+    if (currentTopPosition < standardClosePosition) {
+      /** 현재 위치와 닫히는 위치와의 비교*/
+      const calculatedDiffBreakPositions = [...breakPosition, initPosition].map(
+        (position) => {
+          const calcPosition = getPosition(position, bodyHeight);
+          return {
+            calcPosition,
+            diff: Math.abs(currentTopPosition - calcPosition),
+          };
+        }
+      );
+      const { calcPosition } = calculatedDiffBreakPositions.reduce(
+        (result, item) => (item.diff < result.diff ? item : result),
+        {
+          calcPosition: 0,
+          diff: Number.MAX_SAFE_INTEGER,
+        }
+      );
+      setBSPosition(dragRef, `${calcPosition}px`);
+    } else {
+      onCloseAction && onCloseAction();
+    }
+  };
+
+  useEffect(() => {
+    if (dragRef.current === null) return;
+    dragStateOff();
+    if (isOpen) {
+      setTimeout(() => {
+        document.body.style.setProperty("overflow", "hidden");
+        setBSPosition(dragRef, `${topPosition}px`);
+      }, 100);
+    } else {
+      document.body.style.removeProperty("overflow");
+      setTimeout(() => {
+        dragRef.current?.style.removeProperty("--current-bs-position");
+      }, 100);
+    }
+  }, [isOpen, topPosition]);
+
   return (
-    <BottomSheetContext.Provider value={{ onOpen, onClose, isOpen }}>
+    <BottomSheetContext.Provider
+      value={{
+        isOpen,
+        portalRef,
+        dragRef,
+        dragStateOn,
+        dragStateOff,
+        closeDragElement,
+        topPosition,
+        initPosition,
+        closePosition,
+        breakPosition,
+        bodyHeight,
+        onCloseAction,
+        closeWhenBackdropClick,
+      }}
+    >
       {children}
     </BottomSheetContext.Provider>
   );
@@ -86,132 +208,42 @@ export type BottomSheetPortalProps = {
   closeWhenBackdropClick?: boolean;
 } & HTMLAttributes<HTMLDivElement>;
 
-export const BottomSheet = ({
-  initPosition = "20%",
-  closePosition = "50%",
-  breakPosition = [],
-  closeWhenBackdropClick = true,
-  children,
-  className,
-  ...props
-}: BottomSheetPortalProps) => {
-  /** 페이지의 크기 */
-  const bodyHeight = useScreenSize();
-  const { onClose, isOpen } = useBottomSheetContext();
-  const portalRef = useRef(document.createElement("div"));
-
-  /** control drag state */
-  const divRef = useRef<HTMLDivElement>(null);
-  const dragStateOn = () => {
-    if (divRef.current) divRef.current.dataset.drag = "true";
-  };
-  const dragStateOff = () => {
-    if (divRef.current) divRef.current.dataset.drag = "false";
-  };
-  const topPosition = getPosition(initPosition, bodyHeight);
-
-  const elementDrag = (e: ReactTouchEvent<HTMLDivElement>) =>
-    requestAnimationFrame(() => {
-      if (divRef.current === null) return;
-      const { clientY } = e.touches[0];
-      setBSPosition(
-        divRef,
-        `${clientY < topPosition ? (topPosition + clientY) / 2 : clientY}px`
-      );
-    });
+export const BottomSheet = ({ children }: { children: React.ReactElement }) => {
+  const {
+    portalRef,
+    dragRef,
+    topPosition,
+    onCloseAction,
+    closeDragElement,
+    closeWhenBackdropClick,
+  } = useBottomSheetContext();
 
   const elementMouseDrag = (e: ReactMouseEvent) =>
     requestAnimationFrame(() => {
       e.preventDefault();
-      if (divRef.current === null || divRef.current.dataset.drag === "false")
+      if (
+        !dragRef ||
+        dragRef.current === null ||
+        dragRef.current.dataset.drag === "false"
+      )
         return;
       setBSPosition(
-        divRef,
+        dragRef,
         `${
-          e.clientY < topPosition ? (topPosition + e.clientY) / 2 : e.clientY
+          (e.clientY < topPosition
+            ? (topPosition + e.clientY) / 2
+            : e.clientY) - 20
         }px`
       );
     });
-  const closeDragElement = () => {
-    if (divRef.current === null) return;
-    dragStateOff();
-    /**
-     * ```
-     * 0%: topPosition
-     * 50%: bodySize / 2 + topPostiion / 2
-     * 100%: bodySize
-     * ```
-     */
-    const dividePercentage = Number(closePosition.slice(0, -1)) / 100;
-    /** 현재 위치 */
-    const currentTopPosition = Number(getBSPosition(divRef));
-    /** 바텀 시트가 닫히는 기준 위치 */
-    const standardClosePosition =
-      bodyHeight * dividePercentage + topPosition * (1 - dividePercentage);
-
-    if (currentTopPosition < standardClosePosition) {
-      /** 현재 위치와 닫히는 위치와의 비교*/
-      const calculatedDiffBreakPositions = [...breakPosition, initPosition].map(
-        (position) => {
-          const calcPosition = getPosition(position, bodyHeight);
-          return {
-            calcPosition,
-            diff: Math.abs(currentTopPosition - calcPosition),
-          };
-        }
-      );
-      const { calcPosition } = calculatedDiffBreakPositions.reduce(
-        (result, item) => (item.diff < result.diff ? item : result),
-        {
-          calcPosition: 0,
-          diff: Number.MAX_SAFE_INTEGER,
-        }
-      );
-      setBSPosition(divRef, `${calcPosition}px`);
-    } else {
-      onCloseAction();
-    }
-  };
-
-  const onCloseAction = useCallback(
-    (e?: ReactMouseEvent<HTMLDivElement>) => {
-      if (e && e.target !== e.currentTarget) return;
-      /** 현재 위치 */
-      const currentTopPosition = Number(getBSPosition(divRef));
-      document.body.style.removeProperty("overflow");
-      executeAnimate(
-        currentTopPosition,
-        (position) => (setBSPosition(divRef, `${position}px`), position + 30),
-        (position) => position < document.body.clientHeight
-      );
-      onClose && setTimeout(onClose, 300);
-    },
-    [onClose, divRef]
-  );
-
   useEffect(() => {
-    if (divRef.current === null) return;
-    dragStateOff();
-    if (isOpen) {
-      setTimeout(() => {
-        document.body.style.setProperty("overflow", "hidden");
-        setBSPosition(divRef, `${topPosition}px`);
-      }, 100);
-    } else {
-      document.body.style.removeProperty("overflow");
-      setTimeout(() => {
-        divRef.current?.style.removeProperty("--current-bs-position");
-      }, 100);
-    }
-  }, [isOpen, topPosition]);
-
-  useEffect(() => {
+    if (!portalRef) return;
     const container = portalRef.current;
     if (!hasTarget(container)) {
       document.body.appendChild(portalRef.current);
     }
     const onCloseByEsc = (e: KeyboardEvent) => {
-      e.key === "Escape" && onCloseAction();
+      e.key === "Escape" && onCloseAction && onCloseAction();
     };
     window.addEventListener("keyup", onCloseByEsc);
     return () => {
@@ -220,47 +252,66 @@ export const BottomSheet = ({
       }
       window.removeEventListener("keyup", onCloseByEsc);
     };
-  }, [onCloseAction]);
+  }, [portalRef, onCloseAction]);
 
-  return portalRef.current
+  return portalRef && portalRef.current
     ? createPortal(
         <div
           className={bsStyles.backdrop}
           onMouseUp={closeDragElement}
           onMouseMove={elementMouseDrag}
-          onClick={(e) => closeWhenBackdropClick && onCloseAction(e)}
+          onClick={(e) =>
+            closeWhenBackdropClick && onCloseAction && onCloseAction(e)
+          }
           onMouseLeave={closeDragElement}
         >
-          <div
-            ref={divRef}
-            className={clsx(bsStyles.bottomSheet, className)}
-            {...props}
-          >
-            <div
-              className={bsStyles.handle}
-              onMouseDown={dragStateOn}
-              onTouchStart={dragStateOn}
-              onMouseUp={dragStateOff}
-              onTouchMove={elementDrag}
-              onTouchEnd={closeDragElement}
-            >
-              <span />
-            </div>
-            <div className={bsStyles.children}>{children}</div>
-          </div>
+          {React.cloneElement(children, { ...children.props, ref: dragRef })}
         </div>,
         portalRef.current
       )
     : null;
 };
 
+function BottomSheetHandle({
+  className,
+  children,
+}: React.PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
+  const { dragRef, topPosition, closeDragElement, dragStateOn, dragStateOff } =
+    useBottomSheetContext();
+
+  const elementDrag = (e: ReactTouchEvent<HTMLDivElement>) =>
+    requestAnimationFrame(() => {
+      if (!dragRef || dragRef.current === null) return;
+      const { clientY } = e.touches[0];
+      setBSPosition(
+        dragRef,
+        `${
+          (clientY < topPosition ? (topPosition + clientY) / 2 : clientY) - 20
+        }px`
+      );
+    });
+
+  return (
+    <div
+      className={className}
+      onMouseDown={dragStateOn}
+      onTouchStart={dragStateOn}
+      onMouseUp={dragStateOff}
+      onTouchMove={elementDrag}
+      onTouchEnd={closeDragElement}
+    >
+      {children ?? <span />}
+    </div>
+  );
+}
+
 function BottomSheetCloseButton({
   children,
   ...props
 }: Omit<React.ComponentProps<typeof Button>, "onClick">) {
-  const { onClose } = useBottomSheetContext();
+  const { onCloseAction } = useBottomSheetContext();
   return (
-    <Button onClick={onClose} {...props}>
+    <Button onClick={onCloseAction} {...props}>
       {children}
     </Button>
   );
@@ -280,10 +331,10 @@ function BottomSheetSubmitButton({
   closeOnSubmit,
   ...props
 }: BottomSheetSubmitButtonProps) {
-  const { onClose } = useBottomSheetContext();
+  const { onCloseAction } = useBottomSheetContext();
   const { pending } = useFormStatus();
-  if (closeOnSubmit && pending && onClose) {
-    onClose();
+  if (closeOnSubmit && pending && onCloseAction) {
+    onCloseAction();
   }
   return (
     <Button disabled={pending} {...props}>
@@ -296,6 +347,7 @@ const BS = Object.assign(BottomSheetlMain, {
   Close: BottomSheetCloseButton,
   Submit: BottomSheetSubmitButton,
   BottomSheet,
+  Handle: BottomSheetHandle,
 });
 
 export function hasTarget(container: HTMLDivElement) {
