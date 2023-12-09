e0632df82410e353f37106404534c116e20838e7
ring-wdr <akswnd55@gmail.com>
2023-12-10 07:43:46 +0900

    config: scroll restoration false

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    scrollRestoration: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
