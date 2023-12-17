70d6a313105be6318768a79da02f69dfd3e38681
ring-wdr <akswnd55@gmail.com>
2023-12-17 21:40:04 +0900

    type: fix property on global
    - reading MDN of viewtransition

--- src/type/viewStartTransition.d.ts
+++ src/type/viewStartTransition.d.ts
@@ -1,3 +1,8 @@
+type ViewTransitionReturnType = Record<
+  "updateCallbackDone" | "ready" | "finished",
+  Promise<void>
+>;
+
 interface Document {
-  startViewTransition(callback: () => void): void;
+  startViewTransition(callback: () => void): ViewTransitionReturnType;
 }
