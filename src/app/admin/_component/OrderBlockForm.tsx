029a3c2efbafad8e1686e9aeb493df4b51683f1e
ring-wdr <akswnd55@gmail.com>
2023-12-17 21:41:06 +0900

    change: order block on production

---
+++ src/app/admin/_component/OrderBlockForm.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { toggleOrderState } from "../action";
+
+export default function OrderBlockForm() {
+  const [orderState, orderAction] = useFormState(toggleOrderState, {
+    message: "",
+  });
+  return (
+    <div>
+      <form action={orderAction}>
+        <button> 서버 상태 변경</button>
+      </form>
+      <p>{typeof orderState.status == "boolean" && orderState.message}</p>
+    </div>
+  );
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,19 +1,21 @@
-import Client from "./Client";
-import { getOrderedList } from "@/database/coffeebean/get";
+import Client from "./_component/Client";
+import { getOrderListGroupByUserName } from "@/database/coffeebean/get";
+import OrderBlockForm from "./_component/OrderBlockForm";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
 
 export default async function Admin() {
-  const orderList = await getOrderedList();
+  const orderList = await getOrderListGroupByUserName();
   return (
     <div>
       {process.env.NODE_ENV === "development" ? <Client /> : null}
+      <OrderBlockForm />
       <p>직원들이 주문한 메뉴</p>
       <ul>
         {orderList.map((order, idx) => (
           <li key={idx}>
-            {order.userName}: {order.menuName}
+            {order.userName}: {order.menuName} {order.decaf ? "DECAF" : ""}
           </li>
         ))}
       </ul>
