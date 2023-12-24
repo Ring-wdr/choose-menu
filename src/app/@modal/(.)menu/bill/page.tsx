c419650c41fb28eef2604d754317ab3a405a42af
ring-wdr <akswnd55@gmail.com>
2023-12-24 22:10:04 +0900

    design: parallel modal to bottomsheet

--- src/app/@modal/(.)menu/bill/page.tsx
+++ src/app/@modal/(.)menu/bill/page.tsx
@@ -1,15 +1,10 @@
-import Bill from "@/app/_component/bill";
-import Modal from "@/component/Modal/server";
 import { getOrderListGroupByNameSizeTemp } from "@/database/coffeebean/get";
+import Client from "./client";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const data = await getOrderListGroupByNameSizeTemp();
-  return (
-    <Modal>
-      <Bill data={data} draggable={false} reset={false} />
-    </Modal>
-  );
+  return <Client data={data} />;
 }
