951e268c869712e3e1c382eebc3798120387c92a
ring-wdr <akswnd55@gmail.com>
2024-01-18 21:44:27 +0900

    feat: admin code page

---
+++ src/app/admin/control/action.ts
@@ -0,0 +1,71 @@
+"use server";
+
+import { getAbsenceList } from "@/database/coffeebean/get";
+import { toggleOrderBlock } from "@/database/coffeebean/patch";
+import {
+  crawlAndSaveCategory,
+  crawlAndSaveMenu,
+} from "@/database/coffeebean/post";
+import { ServerActionState } from "@/hooks/useServerAction";
+import { Absence, OrderBlock } from "@/type";
+
+type CategoryCrawlType =
+  | Awaited<ReturnType<typeof crawlAndSaveCategory>>["categoryList"]
+  | string;
+
+export const crawlCategoriesFromExternal = async (
+  _: CategoryCrawlType,
+  data: FormData
+): Promise<CategoryCrawlType> => {
+  const { masterKey } = Object.fromEntries(data);
+  if (masterKey !== process.env.ADMIN_PASSWORD)
+    return "크롤링 권한이 없습니다.";
+  const res = await crawlAndSaveCategory();
+  return res.categoryList;
+};
+
+export const crawlMenuFromExternal = async () => {
+  const res = await crawlAndSaveMenu();
+  return res.menuList;
+};
+
+export const toggleOrderState = async (): Promise<
+  {
+    message: string;
+  } & Partial<OrderBlock>
+> => {
+  const res = await toggleOrderBlock();
+  if (!res) {
+    return { status: false, message: "오류가 있습니다." };
+  }
+  const message =
+    res.status === false
+      ? "메뉴 선택을 막았습니다."
+      : "메뉴 선택 금지를 해제했습니다.";
+
+  return {
+    status: true,
+    message,
+  };
+};
+
+export async function getAbsenceListAction(
+  _: ServerActionState<Absence[]>
+): Promise<ServerActionState<Absence[]>> {
+  try {
+    const absenceList = await getAbsenceList();
+    if (!absenceList || absenceList.length === 0)
+      return { status: "success", message: "결석 인원이 없습니다." };
+    return {
+      status: "success",
+      message: "성공적으로 불러왔습니다.",
+      data: absenceList,
+    };
+  } catch {
+    return { status: "error", message: "메뉴를 불러올 수 없습니다." };
+  }
+}
+
+// export const toggleAbsenceAction = async () => {
+
+// }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,35 @@
-import Client from "./_component/Client";
-import { getOrderListGroupByUserName } from "@/database/coffeebean/get";
-import OrderBlockForm from "./_component/OrderBlockForm";
-import OrderList from "./_component/OrderList";
+"use client";
 
-export const revalidate = 0;
-export const dynamic = "force-dynamic";
+import { useFormState, useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import action from "./action";
+import clsx from "clsx";
 
-export default async function Admin() {
-  const orderList = await getOrderListGroupByUserName();
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return <Button type="submit">{pending ? "Sending" : "Submit"}</Button>;
+}
+
+export default function Page() {
+  const [state, formAction] = useFormState(action, { message: "admin code" });
   return (
-    <div>
-      {process.env.NODE_ENV === "development" ? <Client /> : null}
-      <OrderBlockForm />
-      <OrderList orderList={orderList} />
-    </div>
+    <form
+      action={formAction}
+      className="w-1/2 h-[calc(100%-var(--header-height))] m-auto space-y-4 flex flex-col justify-center"
+    >
+      <Label htmlFor="admin">Welcome to Admin Page</Label>
+      <Input
+        id="admin"
+        type="password"
+        name="admin"
+        placeholder="please input code"
+      />
+      <span className={clsx(state.ok === false && "text-red-500")}>
+        {typeof state.ok !== "undefined" && state.message}
+      </span>
+      <SubmitButton />
+    </form>
   );
 }
