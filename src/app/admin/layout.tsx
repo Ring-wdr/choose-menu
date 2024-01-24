48e4708c07813b017aa9f769044f5406afec29b0
ring-wdr <akswnd55@gmail.com>
2024-01-25 01:12:47 +0900

    change: to be static page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,17 +1,12 @@
-import { headers } from 'next/headers';
-
 import AdminSideBar from './sidebar';
 
 type AdminLayoutProps = {
   children: React.ReactNode;
 };
 export default function Layout({ children }: AdminLayoutProps) {
-  const referer = headers().get('referer');
-  const isAdminRoot =
-    typeof referer === 'string' && new URL(referer).pathname === '/admin';
   return (
     <div className="flex h-[calc(100%-var(--header-height))]">
-      {!isAdminRoot && <AdminSideBar />}
+      <AdminSideBar />
       {children}
     </div>
   );
--- src/app/admin/sidebar.tsx
+++ src/app/admin/sidebar.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
 import { TriangleLeftIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
 
@@ -6,16 +9,32 @@ import { Button } from '@/components/ui/button';
 const pathnames = ['control', 'menu', 'user'];
 
 export default function AdminSideBar() {
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const checkboxRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        sidebarRef.current &&
+        checkboxRef.current &&
+        !sidebarRef.current.contains(e.target as Node)
+      ) {
+        checkboxRef.current.checked = true;
+      }
+    };
+    window.addEventListener('mousedown', handleClick);
+    return () => window.removeEventListener('mousedown', handleClick);
+  }, []);
   return (
-    <div className="relative flex h-full z-10">
+    <div className="relative flex h-full z-10" ref={sidebarRef}>
       <input
         type="checkbox"
         id={'admin-sidebar'}
         className="peer/check"
         defaultChecked
         hidden
+        ref={checkboxRef}
       />
-      <div className="absolute w-48 bg-primary h-full peer-checked/check:w-0 peer-checked/check:[&>*]:hidden gap-3 transition-all">
+      <div className="absolute w-48 bg-primary da h-full peer-checked/check:w-0 peer-checked/check:[&>*]:hidden gap-3 transition-all invert">
         {pathnames.map((pathname) => (
           <Link key={pathname} href={`/admin/${pathname}`}>
             <Button className="w-full peer-checked/check:hidden rounded-none">
@@ -26,7 +45,7 @@ export default function AdminSideBar() {
       </div>
       <label
         htmlFor="admin-sidebar"
-        className="absolute -top-10 -right-8 bg-slate-200 rounded-full peer-checked/check:rotate-180  hover:cursor-pointer transition-all"
+        className="absolute -top-10 -right-8 bg-slate-200 rounded-full peer-checked/check:rotate-180  hover:cursor-pointer transition-all invert"
       >
         <TriangleLeftIcon width={30} height={30} />
       </label>
