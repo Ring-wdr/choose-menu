f8ba164cae0923e948de6fb1addd533267febe98
ring-wdr <akswnd55@gmail.com>
2024-01-25 23:23:17 +0900

    trivial: a little css

--- src/app/admin/control/_component/OrderBlockForm.tsx
+++ src/app/admin/control/_component/OrderBlockForm.tsx
@@ -11,11 +11,13 @@ export default function OrderBlockForm() {
     message: '',
   });
   return (
-    <div>
-      <form action={orderAction}>
-        <Button> 서버 상태 변경</Button>
+    <div className="w-1/2 mx-auto mt-5">
+      <form action={orderAction} className="flex flex-col">
+        <Button size={'lg'}> 서버 상태 변경</Button>
       </form>
-      <p>{typeof orderState.status == 'boolean' && orderState.message}</p>
+      <p className="flex justify-center mt-5">
+        {typeof orderState.status == 'boolean' && orderState.message}
+      </p>
     </div>
   );
 }
